bd69bf215f83648ee7165f535b13b2df1422ceda
differentdeepak <dk20071993@gmail.com>
2024-08-30 12:57:29 +0530

    menu

---
+++ README.md
@@ -0,0 +1,3 @@
+## Full Screen Hover Menu
+
+> This is made with jquery or css3.
---
+++ main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+    $('body').addClass('light');
+    $('ul.menu li').mouseover(function () {
+        $('body').addClass('dark').removeClass('light');
+    });
+    $('ul.menu li').mouseout(function () {
+        $('body').addClass('light').removeClass('dark');
+    });
+
+});
+
